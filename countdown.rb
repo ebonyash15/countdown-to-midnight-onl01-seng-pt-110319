0defd21ad7e3ea78e1b4722f05549ee7596297af
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-06 14:45:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(numnber)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
